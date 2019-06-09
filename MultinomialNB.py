f39b503f747d984a95fd1b1f4844d9a9cda15e96
Wu Changrong <wcr@live.cn>
2019-06-09 16:29:36 +0800

    start exp

---
+++ MultinomialNB.py
@@ -0,0 +1,42 @@
+import numpy as np
+from sklearn.naive_bayes import MultinomialNB
+
+import preprocess as prep
+
+def train_MulNB(data, labels, class_prior=None):
+    mnb = MultinomialNB(alpha=2.0, fit_prior=True, class_prior=class_prior)
+    mnb.fit(data, labels)
+
+    return mnb
+
+def predict_MulNB(data, model):
+    return model.predict(data)
+
+def score_MulNB(data, labels, model):
+    return model.score(data, labels)
+
+def experiment(train_dataset, test_dataset, train_labels, test_labels=None):
+    total_dataset = train_dataset + test_dataset
+    total_dataset = prep.eliminate_noise(total_dataset, "，。\t  “”；")
+    seg_dataset = prep.seg_words(total_dataset)
+    vec_dataset = prep.word_to_vec(seg_dataset, output_path="./dataset/word2vec.model")
+
+    vec_train_dataset = vec_dataset[0:len(test_dataset)]
+    vec_test_dataset = vec_dataset[len(test_dataset):]
+
+    MulNB_model = train_MulNB(vec_train_dataset, train_labels)
+    res = predict_MulNB(vec_test_dataset, MulNB_model)
+    if test_labels != None:
+        print("accuracy: {0}".format(score_MulNB(test_dataset, test_labels, MulNB_model)))
+
+    return res
+
+if __name__ == "__main__":
+    dataset, labels, test_dataset, class_num = prep.load_raw_data()
+
+    train_dataset = dataset[0:700000]
+    predict_dataset = dataset[700000:]
+    train_labels = labels[0:700000]
+    predict_labels = labels[700000:]
+
+    res = experiment(train_dataset, predict_dataset, train_labels, predict_labels)
--- preprocess.py
+++ preprocess.py
@@ -11,95 +11,87 @@ All dataset will be stored as list([sentence, label]) and output as list([label,
 
 def load_raw_data(output_path=None):
     data_file = open("./dataset/train.data", encoding="utf-8-sig")
+    test_file = open("./dataset/test.data", encoding="utf-8-sig")
     label_file = open("./dataset/train.solution", encoding="utf-8-sig")
     emoji_file = open("./dataset/emoji.data", encoding="utf-8-sig")
 
     train_data = data_file.readlines()
+    test_data = test_file.readlines()
     train_label = label_file.readlines()
     emoji = emoji_file.readlines()
+
     emoji_map = {}
     dataset = []
+    test_dataset = []
+    labels = []
 
     #construct emoji map: string -> id
     for line in emoji:
         content = str(line).split()
         emoji_map[content[1]] = content[0]
 
-    #combine files to construct dataset
+    #combine files to construct dataset and labels
     if len(train_data) != len(train_label):
         print("Invalid data, may be stained")
     for lineno in range(len(train_data)):
         emoji_name = str(train_label[lineno]).rstrip('\n').rstrip('}').lstrip('{')
-        instance = [train_data[lineno].strip(), int(emoji_map[emoji_name])]
-        dataset.append(instance)
+        dataset.append(train_data[lineno].strip())
+        labels.append(int(emoji_map[emoji_name]))
 
-    #write the dataset into disk file
+    #write the labeled_dataset into disk file
     if output_path != None:
         output_file = open(output_path, "w", encoding="utf-8")
-        for instance in dataset:
-            output_file.write(str(instance[1]) + " " + instance[0] + "\n")
+        for i in range(len(dataset)):
+            output_file.write(str(labels[i]) + " " + dataset[i] + "\n")
         
         output_file.close()
 
+    #load test data
+    for line in test_data:
+        sentence = line.split(" ", 1)
+        test_dataset.append(sentence)
+
     data_file.close()
+    test_file.close()
     label_file.close()
     emoji_file.close()
-    return dataset, len(emoji_map.keys())
+    return dataset, labels, test_dataset, len(emoji_map.keys())
 
-def class_statistic(dataset, class_num):
+def class_statistic(labels, class_num):
     counters = {}
     for i in range(class_num):
         counters[i] = 0
 
     #count instances for every class
-    for instance in dataset:
-        counters[instance[1]] = counters[instance[1]] + 1
+    for l in labels:
+        counters[l] = counters[l] + 1
 
     return counters
 
 def eliminate_noise(dataset, noise_set):
     new_dataset = []
-    for instance in dataset:
-        sentence = str(instance[0])
+    for sentence in dataset:
         for char in noise_set:
             sentence = sentence.strip(char)
         
-        new_instance = [sentence, instance[1]]
-        new_dataset.append(new_instance)
+        new_sentence = sentence
+        new_dataset.append(new_sentence)
     
     return new_dataset
 
 def seg_words(dataset, output_path=None):
     new_dataset = []
     for instance in dataset:
-        words = list(jieba.cut(instance[0]))
-        new_instance = [words, instance[1]]
+        words = list(jieba.cut(instance))
+        new_instance = words
         new_dataset.append(new_instance)
-
-    #write the new dataset into disk file
-    if output_path != None:
-        output_file = open(output_path, "w", encoding="utf-8")
-        for new_instance in new_dataset:
-            #output the label
-            output_file.write(str(new_instance[1]) + " ")
-
-            #output the words
-            for word in new_instance[0]:
-                output_file.write(str(word) + " ")
-            output_file.write("\n")
-        
-        output_file.close()
 
     return new_dataset
 
 def word_to_vec(seg_dataset, input_path=None, output_path=None):
-    seg_sentences = []
-    for instance in seg_dataset:
-        seg_sentences.append(instance[0])
-
     # load or train the model
     if input_path == None:
-        model = Word2Vec(seg_sentences, workers=4, window=4, size=100, min_count=0, sg=0, hs=0)
+        model = Word2Vec(seg_dataset, workers=4, window=4, size=100, min_count=0, sg=0, hs=0)
     else:
         model = Word2Vec.load(input_path)
 
@@ -109,27 +101,26 @@ def word_to_vec(seg_dataset, input_path=None, output_path=None):
 
     #compute the max length of each sentence
     max_len = 0
-    for sentence in seg_sentences:
+    for sentence in seg_dataset:
         if len(sentence) > max_len:
             max_len = len(sentence)
 
     #construct new dataset
     new_dataset = []
-    for i in range(len(seg_sentences)):
+    for i in range(len(seg_dataset)):
         #construct the word vectors for a sentence, supplement missing tails
         word_vecs = []
-        for word in seg_sentences[i]:
+        for word in seg_dataset[i]:
             word_vecs.append(model.wv[word])
-        for j in range(len(seg_sentences), max_len):
-            word_vec.append(np.array([0] * 100))
+        for j in range(len(seg_dataset[i]), max_len):
+            word_vecs.append(np.array([0] * 100))
 
-        new_instance = [word_vecs, seg_dataset[i][1]]
-        new_dataset.append(new_instance)
+        new_dataset.append(word_vecs)
 
     return new_dataset
 
 if __name__ == "__main__":
-    dataset, class_num = load_raw_data()
+    dataset, labels, test_dataset, class_num = load_raw_data()
 
     dataset = eliminate_noise(dataset, "，。\t  “”；")
 
